d9970d5be4e9aeb2aa9e453486aeedcd93a5f48e
Hanzou Urushihara <61415393+im-hanzou@users.noreply.github.com>
2022-11-08 06:15:40 +0700

    Create laravel-debugbar.py

---
+++ laravel-debugbar.py
@@ -0,0 +1,63 @@
+import os, requests, time
+from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Pool
+import threading
+import sys
+from colorama import Fore, Style
+
+
+def screen_clear():
+    _ = os.system('cls')
+
+
+bl = Fore.BLUE
+wh = Fore.WHITE
+gr = Fore.GREEN
+red = Fore.RED
+res = Style.RESET_ALL
+yl = Fore.YELLOW
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0'}
+
+def debugbar (star):
+    if "://" in star:
+      star = star
+    else:
+      star = "http://" + star
+    star = star.replace('\n', '').replace('\r', '')
+    url = star + "/"
+    check = requests.get(url, headers=headers, timeout=3)
+    resp = check.text
+    try:
+        if "PhpDebugBar.DebugBar" in resp:
+            print(f"Debugbar {gr}OK{res} => {star}\n")
+            mrigel = open("found.txt", "a")
+            mrigel.write(f'{star}\n')
+        else:
+            print(f"{red}Not Found{res} Debugbar => {star}\n")
+    except:
+        pass
+
+
+def filter(star):
+    try:
+       debugbar(star)
+    except:
+       pass
+
+
+def main():
+    print(f'''{gr}[ DEBUDBAR CHECKER ] | [ BY XNXXVIDOES ]''')
+    list = input(f"{gr}Give Me Your List.txt/{red}XNXXVIDOES> {gr}${res} ")
+    star = open(list, 'r').readlines()
+    try:
+       ThreadPool = Pool(50)
+       ThreadPool.map(filter, star)
+       ThreadPool.close()
+       ThreadPool.join()
+    except:
+       pass
+       
+if __name__ == '__main__':
+    screen_clear()
+    main()
